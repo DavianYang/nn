7122364bb3abf801797c9b27c15b0569bfadf2ae
DavianYang <kothantyarzarhein@gmail.com>
2020-08-25 21:08:28 +0630

    [Add] DenseNet

--- densenet/densenet.py
+++ densenet/densenet.py
@@ -6,6 +6,7 @@ import torchvision.transforms as T
 import pytorch_lightning as pl
 
 import math
+from collections import OrderedDict
 
 class ConvBlock(nn.Module):
     """
@@ -17,16 +18,16 @@ class ConvBlock(nn.Module):
     """
     def __init__(
         self, 
-        in_channels, 
-        growthrate,
-        bn_size, 
-        drop_rate
+        in_features: int, 
+        growthrate: float,
+        bn_size: int, 
+        drop_rate: float
         ):
         super(ConvBlock, self).__init__()
         # 1x1 Convolution
-        self.add_module('bn1', nn.BatchNorm2d(in_channels))
+        self.add_module('bn1', nn.BatchNorm2d(in_features))
         self.add_module('act1', nn.ReLU(inplace=True))
-        self.add_module('conv1', nn.Conv2d(in_channels, bn_size * growthrate, kernel_size=1, stride=1, bias=False) )
+        self.add_module('conv1', nn.Conv2d(in_features, bn_size * growthrate, kernel_size=1, stride=1, bias=False) )
         
         # 3x3 Convolution
         self.add_module('bn2', nn.BatchNorm2d(bn_size * growthrate))
@@ -55,13 +56,13 @@ class TransitionBlock(nn.Module):
     """
     def __init__(
         self,
-        in_channels,
-        out_channels):
+        in_features: int,
+        out_features: int):
         super().__init__()
         
-        self.add_module('bn', nn.BatchNorm2d(in_channels))
+        self.add_module('bn', nn.BatchNorm2d(in_features))
         self.add_module('act', nn.ReLU(inplace=True))
-        self.add_module('conv', nn.Conv2d(in_channels, out_channels, kernel_size=1, stride=1, bias=False))
+        self.add_module('conv', nn.Conv2d(in_features, out_features, kernel_size=1, stride=1, bias=False))
         self.add_module('pool', nn.AvgPool2d(kernel_size=2, stride=2))
         
 
@@ -72,7 +73,13 @@ class DenseBlock(nn.ModuleDict):
         Return:
             Output tensor for the block
     """
-    def __init__(self, num_layers, in_features, bn_size, growthrate, drop_rate):
+    def __init__(
+        self, 
+        num_layers: int, 
+        in_features: int, 
+        bn_size: int, 
+        growthrate: float, 
+        drop_rate: float):
         super().__init__()
         for i in range(num_layers):
             layer = ConvBlock(
@@ -89,3 +96,61 @@ class DenseBlock(nn.ModuleDict):
             new_features = layer(features)
             features.append(new_features)
         return torch.cat(features, 1)
+    
+
+class DenseNet(nn.Module):
+    def __init__(
+        self, 
+        growthrate=32, 
+        block_config=(6, 12, 24, 16), 
+        num_init_features=64, bn_size=4, 
+        drop_rate=0, 
+        num_classes=1000):
+        super().__init__()
+        
+        # 1st Conv
+        self.features = nn.Sequential(OrderedDict([
+            ('conv0', nn.Conv2d(3, num_init_features, kernel_size=7, stride=2, padding=3, bias=False)),
+            ('bn0', nn.BatchNorm2d(num_init_features)),
+            ('act0', nn.ReLU(inplace=True)),
+            ('pool0', nn.MaxPool2d(kernel_size=3, stride=2, padding=1))
+        ]))
+        
+        num_features = num_init_features
+        for i, num_layers in enumerate(block_config):
+            dense_block = DenseBlock(
+                num_layers=num_layers,
+                in_features=num_features,
+                bn_size=bn_size,
+                growthrate=growthrate,
+                drop_rate=drop_rate
+            )
+            self.features.add_module(f'convblock{i+1}', dense_block)
+            num_features = num_features + num_layers * growthrate
+            
+            if i != len(block_config) - 1:
+                transition = TransitionBlock(in_features=num_features, out_features=num_features // 2)
+                self.features.add_module(f'transition{i+1}', transition)
+                num_features = num_features // 2
+                
+            self.features.add_module('bn5', nn.BatchNorm2d(num_features))
+            
+            self.classifer = nn.Linear(num_features, num_classes)
+            
+            for m in self.modules():
+                if isinstance(m, nn.BatchNorm2d):
+                    nn.init.kaiming_normal_(m.weight)
+                elif isinstance(m, nn.BatchNorm2d):
+                    nn.init.constant_(m.bias, 0)
+                elif isinstance(m, nn.Linear):
+                    nn.init.constant_(m.bias, 0)
+                    
+    
+    def forward(self, x):
+        features = self.features(x)
+        out = F.relu(features, inplace=True)
+        out = F.adaptive_avg_pool2d(out, (1, 1))
+        out = torch.flatten(out, 1)
+        out = self.classifer(out)
+        return out
+    
