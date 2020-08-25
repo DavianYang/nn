ef130e9fba9ac143495be7b07c13a3eed4b1abb5
DavianYang <kothantyarzarhein@gmail.com>
2020-08-25 19:07:35 +0630

    [Add] ConvBlock -> DenseNet

--- densenet/densenet.py
+++ densenet/densenet.py
@@ -5,6 +5,8 @@ import torch.nn.functional as F
 import torchvision.transforms as T
 import pytorch_lightning as pl
 
+import math
+
 class ConvBlock(nn.Module):
     """
         A building block of Conv Block
@@ -16,50 +18,28 @@ class ConvBlock(nn.Module):
     def __init__(
         self, 
         in_channels, 
-        growthrate):
-        super().__init__()
-        interchannels = 4 * growthrate
-        
+        growthrate,
+        bn_size, 
+        drop_rate
+        ):
+        super(ConvBlock, self).__init__()
         # 1x1 Convolution
-        self.conv1x1 = nn.ModuleDict({
-            'bn': nn.BatchNorm2d(in_channels),
-            'conv': nn.Conv2d(in_channels, interchannels, kernel_size=1, stride=1, bias=False)  
-        })
+        self.add_module('bn1', nn.BatchNorm2d(in_channels))
+        self.add_module('act1', nn.ReLU(inplace=True))
+        self.add_module('conv1', nn.Conv2d(in_channels, bn_size * growthrate, kernel_size=1, stride=1, bias=False) )
         
         # 3x3 Convolution
-        self.conv3x3 = nn.ModuleDict({
-            'bn': nn.BatchNorm2d(interchannels),
-            'conv': nn.Conv2d(interchannels, growthrate, kernel_size=3, stride=1, padding=1, bias=False)
-        })
+        self.add_module('bn2', nn.BatchNorm2d(bn_size * growthrate))
+        self.add_module('act2', nn.ReLU(inplace=True))
+        self.add_module('conv2', nn.Conv2d(bn_size * growthrate, growthrate, kernel_size=3, stride=1, padding=1, bias=False))
+    
+        self.drop_out = float(drop_rate)
         
         
-    def forward(self, x):
-        out = self.conv1x1['conv'](F.relu(self.conv1x1['bn'](x)))
-        out = self.conv3x3['conv'](F.relu(self.conv3x3['bn'](out)))
-        out = torch.cat((x, out), 1)
-        return out
-
-
-class TransitionBlock(nn.Module):
-    """
-        A building block of Transition Block
-        
-        Return:
-            Output tensor for the block
-
-    """
-    def __init__(
-        self,
-        in_channels,
-        out_channels):
-        super().__init__()
-        
-        self.transition = nn.ModuleDict({
-            'bn': nn.BatchNorm2d(in_channels),
-            'conv': nn.Conv2d(in_channels, out_channels, kernel_size=1, stride=1, bias=False)
-        })
-        
-        
-    def forward(self, x):
-        out = F.avg_pool2d(self.transition['conv'](F.relu(self.transition['bn'](x))))
+    def forward(self, inputs):
+        out = torch.cat(inputs, 1)
+        out = self.conv1(self.act1(self.bn1(out)))
+        out = self.conv2(self.act2(self.bn2(out)))
+        if self.drop_out > 0:
+            out = F.dropout(out, p=self.drop_out)
         return out
