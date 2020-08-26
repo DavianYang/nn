ce2afa3207562825cb912c30c5d586fb18f28a3d
DavianYang <kothantyarzarhein@gmail.com>
2020-08-26 20:24:04 +0630

    [Add] DenseNetLightning

--- densenet/densenet.py
+++ densenet/densenet.py
@@ -1,10 +1,12 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+import torch.optim as optim
 
 import torchvision.transforms as T
 import pytorch_lightning as pl
 from pytorch_lightning.core.memory import ModelSummary
+from pytorch_lightning.metrics.functional import accuracy
 
 import math
 from collections import OrderedDict
@@ -162,6 +164,82 @@ class DenseNet(nn.Module):
         out = self.classifer(out)
         return out
 
+class DenseNetLightning(pl.LightningModule):
+    def __init__(
+        self,
+        model: nn.Module
+    ):
+        self.model = model
+    
+    def forward(self, x):
+        return self.model(x)
+    
+    def configure_optimizers(self):
+        return optim(
+            self.parameters(),
+            lr=1e-3,
+            weight_decay=0
+        )
+        
+    def training_step(self, batch, batch_idx):
+        img, target = batch
+        pred = self(img)
+        loss = F.cross_entropy(pred, target)
+        acc = accuracy(F.log_softmax(pred, dim=1), target)
+        
+        return {
+            "loss": loss,
+            "acc": acc
+        }
+        
+    def training_epoch_end(self, outputs):
+        avg_loss = torch.stack([x["loss"] for x in outputs]).mean()
+        avg_acc = torch.stack([x["acc"] for x in outputs]).mean()
+        
+        return {
+            "train_epoch_loss": avg_loss,
+            "train_epoch_acc": avg_acc    
+        }
+        
+    def validation_step(self, batch, batch_idx):
+        img, target = batch
+        pred = self(img)
+        loss = F.cross_entropy(pred, target)
+        acc = accuracy(F.log_softmax(pred, dim=1), target)
+        
+        return {
+            "val_loss": loss,
+            "val_acc": acc
+        }
+        
+    def validation_epoch_end(self, outputs):
+        avg_loss = torch.stack([x["val_loss"] for x in outputs]).mean()
+        avg_acc = torch.stack([x["val_acc"] for x in outputs]).mean()
+        
+        return {
+            "val_epoch_loss": avg_loss,
+            "val_epoch_acc": avg_acc
+        }
+        
+    def test_step(self, batch, batch_idx):
+        img, target = batch
+        pred = self(img)
+        loss = F.cross_entropy(pred, target)
+        acc = accuracy(F.log_softmax(pred, dim=1), target)
+        
+        return {
+            "test_loss": loss,
+            "test_acc": acc
+        }
+        
+    def test_epoch_end(self, outputs):
+        avg_loss = torch.stack([x["test_loss"] for x in outputs]).mean()
+        avg_acc = torch.stack([x["test_acc"] for x in outputs]).mean()
+        
+        return {
+            "test_epoch_loss": avg_loss,
+            "test_epoch_acc": avg_acc
+        }
 
 if __name__ == "__main__":
     model = DenseNet()
